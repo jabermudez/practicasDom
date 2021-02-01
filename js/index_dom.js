f03eb2ae30e0fe064058eee837681a33463ef974
José Alexander Bermudez <jabermudez99@misena.edu.co>
2021-01-31 22:57:15 -0500

    Reloj y ]Alarma JS
    Se codifica un reloj y alarma con sus respectivos botones para iniciar y detener.

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,7 +1,11 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
+import {digitalClock, alarm} from "./reloj.js";
+
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded",(e)=>{
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
+    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+    alarm("assets/alarma1.mp3", "#activar-alarma", "#desactivar-alarma");
 });
