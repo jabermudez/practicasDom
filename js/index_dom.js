9c6d60a659d4fbb201c16698620de86371011c87
José Alexander Bermudez <jabermudez99@misena.edu.co>
2021-02-01 16:12:36 -0500

    Eventos del Teclado
    Creando shortcuts en JavaScript

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,6 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
 import {digitalClock, alarm} from "./reloj.js";
-
+import{shortcuts} from "./teclado.js";
 
 const d = document;
 
@@ -9,3 +9,7 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm("assets/alarma1.mp3", "#activar-alarma", "#desactivar-alarma");
 });
+
+d.addEventListener("keydown", (e) => {
+    shortcuts(e);
+})
