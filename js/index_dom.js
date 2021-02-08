c9f407828b7f07cd253fd0ce694bcb0b9f3bd264
José Alexander Bermudez <jabermudez99@misena.edu.co>
2021-02-08 00:00:41 -0500

    Cuenta Regresitva
    Conteo regresivo para cualquier fecha en especial realizado en JavaScript

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,7 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
 import {digitalClock, alarm} from "./reloj.js";
 import{moveBall, shortcuts} from "./teclado.js";
+import countdown from "./cuenta_regresiva.js";
 
 const d = document;
 
@@ -8,6 +9,7 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm("assets/alarma1.mp3", "#activar-alarma", "#desactivar-alarma");
+    countdown("countdown", "Oct 05 2021 00:00:00", "Feliz Cumpleaños Alitox 🥰🥰🥰");
 });
 
 d.addEventListener("keydown", (e) => {
