8e7f08f7b1ae9d94f69077278f648d70a8d608a8
José Alexander Bermudez <jabermudez99@misena.edu.co>
2021-02-08 22:04:44 -0500

    Boton Scroll
    Realización botón de Scroll para manejar un landingpage

--- js/index_dom.js
+++ js/index_dom.js
@@ -2,6 +2,7 @@ import hamburgerMenu from "./menu_hamburguesa.js";
 import {digitalClock, alarm} from "./reloj.js";
 import{moveBall, shortcuts} from "./teclado.js";
 import countdown from "./cuenta_regresiva.js";
+import scrollTopButton from "./boton_scroll.js";
 
 const d = document;
 
@@ -10,6 +11,7 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm("assets/alarma1.mp3", "#activar-alarma", "#desactivar-alarma");
     countdown("countdown", "Oct 05 2021 00:00:00", "Feliz Cumpleaños Alitox 🥰🥰🥰");
+    scrollTopButton(".scroll-top-btn");
 });
 
 d.addEventListener("keydown", (e) => {
