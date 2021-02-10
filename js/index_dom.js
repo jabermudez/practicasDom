b52e36907fc3db63b51ac99f92c9c536929ca1ee
José Alexander Bermudez <jabermudez99@misena.edu.co>
2021-02-09 22:55:28 -0500

    Tema Dark
    Cambio de color en el tema con JavaScript

--- js/index_dom.js
+++ js/index_dom.js
@@ -3,6 +3,7 @@ import {digitalClock, alarm} from "./reloj.js";
 import{moveBall, shortcuts} from "./teclado.js";
 import countdown from "./cuenta_regresiva.js";
 import scrollTopButton from "./boton_scroll.js";
+import darkTheme from "./tema_oscuro.js";
 
 const d = document;
 
@@ -12,6 +13,8 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     alarm("assets/alarma1.mp3", "#activar-alarma", "#desactivar-alarma");
     countdown("countdown", "Oct 05 2021 00:00:00", "Feliz Cumpleaños Alitox 🥰🥰🥰");
     scrollTopButton(".scroll-top-btn");
+    darkTheme(".dark-theme-btn", "dark-mode");
+    
 });
 
 d.addEventListener("keydown", (e) => {
