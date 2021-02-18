a253057eee52d5094743030abb17f7d7ed22b526
José Alexander Bermudez <jabermudez99@misena.edu.co>
2021-02-17 23:24:31 -0500

    Responsive Tester
    Se realiza un Script para testear página y darle un tamaño predefinido y poder cerrarlo a través del Script

--- js/index_dom.js
+++ js/index_dom.js
@@ -5,6 +5,7 @@ import countdown from "./cuenta_regresiva.js";
 import scrollTopButton from "./boton_scroll.js";
 import darkTheme from "./tema_oscuro.js";
 import responsiveMedia from "./objeto_responsive.js";
+import responsiveTester from "./prueba_responsive.js";
 
 const d = document;
 
@@ -27,6 +28,8 @@ d.addEventListener("DOMContentLoaded",(e)=>{
         `<a href="https://goo.gl/maps/9o7fnH8rwwQwsyNZ9" target="_blank" rel="noopener">Ver Mapa</a>`,
         `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.252147895275!2d-75.65967070804834!3d4.548588301532981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e38f4fb8f430bbd%3A0x159c0ed6d96d65f!2sParque%20De%20La%20Vida!5e0!3m2!1ses!2sco!4v1613448959615!5m2!1ses!2sco" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`
         );
+    
+    responsiveTester("responsive-tester");
     
 });
 
