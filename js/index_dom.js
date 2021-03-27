11fff4ce709a92ab9332a1f31ae39cee34f04b1f
José Alexander Bermudez <jabermudez99@misena.edu.co>
2021-03-26 21:30:24 -0500

    Detección Camara con JS
    Ejercicio práctico para detección de la web cam del computador.

--- js/index_dom.js
+++ js/index_dom.js
@@ -8,6 +8,7 @@ import responsiveMedia from "./objeto_responsive.js";
 import responsiveTester from "./prueba_responsive.js";
 import userDeviceInfo from "./deteccion_dispositivos.js";
 import networkStatus from "./deteccion_red.js";
+import webCam from "./deteccion_webcam.js";
 
 const d = document;
 
@@ -34,6 +35,8 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     responsiveTester("responsive-tester");
 
     userDeviceInfo("user-device");
+
+    webCam("webcam");
     
 });
 
