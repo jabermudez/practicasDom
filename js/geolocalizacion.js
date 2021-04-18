7960c8658cf0ce806e22e160159d3938b2e2be80
José Alexander Bermudez <jabermudez99@misena.edu.co>
2021-04-17 23:02:54 -0500

    Geolocalizacion
    Codigo JS para realizar geolocalizaciòn

---
+++ js/geolocalizacion.js
@@ -0,0 +1,34 @@
+const d=document,
+ n=navigator;
+
+export default function getGeolocation(id) {
+    const $id = d.getElementById(id),
+     options = {
+         enableHighAccuracy: true,
+         timeout: 5000,
+         maximumAge:0
+     };
+
+    const success = postion => {
+        let coords = postion.coords;
+        console.log(postion);
+
+        $id.innerHTML = `
+        <p>Tu posición actual es: </p>
+        <ul>
+            <li>Latitud: <b>${coords.latitude}</b></li>
+            <li>Longitud: <b>${coords.longitude}</b></li>
+            <li>Precisión: <b>${coords.accuracy}</b>Metros</li>
+         </ul>
+         <a href="https://www.google.com/maps/@${coords.latitude},${coords.longitude},20z"
+         target="_blank" rel="noopener">Ver en Google Maps</a>
+        `;
+    };
+    
+    const error = (err) => {        
+        $id.innerHTML = `<p> <mark> Error ${err.code}: ${err.message}</mark></p>`;
+        console.log(`Error ${err.code}: ${err.message}`)
+    };   
+
+    n.geolocation.getCurrentPosition(success, error, options)
+};
--- js/index_dom.js
+++ js/index_dom.js
@@ -9,6 +9,7 @@ import responsiveTester from "./prueba_responsive.js";
 import userDeviceInfo from "./deteccion_dispositivos.js";
 import networkStatus from "./deteccion_red.js";
 import webCam from "./deteccion_webcam.js";
+import getGeolocation from "./geolocalizacion.js";
 
 const d = document;
 
@@ -37,6 +38,7 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     userDeviceInfo("user-device");
 
     webCam("webcam");
+    getGeolocation("geolocation");  
     
 });
 
