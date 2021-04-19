ffa63d5a858dacfbcbdcaa670be5cd85c281b2af
José Alexander Bermudez <jabermudez99@misena.edu.co>
2021-04-18 22:23:45 -0500

    Filtro de Busqueda JS
    Permite filtrar imágenes y mostrarlas según sus nombres

--- js/index_dom.js
+++ js/index_dom.js
@@ -10,6 +10,7 @@ import userDeviceInfo from "./deteccion_dispositivos.js";
 import networkStatus from "./deteccion_red.js";
 import webCam from "./deteccion_webcam.js";
 import getGeolocation from "./geolocalizacion.js";
+import searchFilters from "./filtro_busquedas.js";
 
 const d = document;
 
@@ -38,7 +39,8 @@ d.addEventListener("DOMContentLoaded",(e)=>{
     userDeviceInfo("user-device");
 
     webCam("webcam");
-    getGeolocation("geolocation");  
+    getGeolocation("geolocation");
+    searchFilters(".card-filter", ".card");
     
 });
 
